c5b1a835ff0359fd3af011cad53c490627407a42
kakanghosh <kakanghosh69@gmail.com>
2018-07-18 22:39:13 +0600

    Refine datas

--- python3/Banglish.py
+++ python3/Banglish.py
@@ -4,34 +4,15 @@ import enchant
 from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import os
+from googletrans import Translator
 
-result = []
+textBlobresult = []
+vaderSentimentResult = []
 
 def showLines(lines):
     for line in lines:
         print(line)
 
-def removeLinks(line):
-    text = re.sub(r'http\S+', '', line)
-    return text
-
-def removeSpecialChar(line):
-    text = re.sub(r'[?|$|.|!|*]', r'', line)
-    return text
-
-def getUniqueReviews(lines):
-    uniqueLines = []
-    foundNewLine = False
-    for line in lines:
-        if len(line.strip()) == 0 and foundNewLine is False:
-            uniqueLines.append("")
-            foundNewLine = True
-        elif line not in uniqueLines:
-            line = removeLinks(line)
-            line = removeSpecialChar(line)
-            uniqueLines.append(line)
-            foundNewLine = False
-    return uniqueLines
 
 def banglishToBangla(lines):
     banglas = []
@@ -43,32 +24,19 @@ def isEnglish(word):
     d = enchant.Dict("en_US")
     return d.check(word)
 
-def getReviews(lines):
-    lines = getUniqueReviews(lines)
-    review = ""
-    reviewList = []
-    for line in lines:
-        if len(line.strip()) == 0:
-            reviewList.append(review)
-            review = ""
-        else:
-            review += line + "\n"
-    return reviewList
 
-def sentimentAnalysis(lines):
-    print(lines)
-    textBlobTrans = TextBlob(avro.parse(lines))
-    lines = str(textBlobTrans.translate(to='en'))
-    #lines = str("Good")
-    # #print(lines)
-    analyze = TextBlob(lines)
-    #print('By TextBlob Sentiment analysis')
-    #print(analyze.sentiment)
-    #
-    # print('By vaderSentiment analyzer')
-    # analyzer = SentimentIntensityAnalyzer()
-    # vs = analyzer.polarity_scores(lines)
-    # result.append(vs)
+
+def sentimentAnalysis(linesCollection):
+    if len(linesCollection.strip()) is not 0:
+        translator = Translator()
+        lines = translator.translate(avro.parse(linesCollection))
+        #print(lines.text)
+        analyze = TextBlob(str(lines.text))
+        textBlobresult.append(analyze.sentiment)
+        #
+        # analyzer = SentimentIntensityAnalyzer()
+        # vs = analyzer.polarity_scores(lines)
+        # vaderSentimentResult.append(vs)
 
 def splitByNewLine(text):
 
@@ -87,16 +55,23 @@ def splitByNewLine(text):
     # return newLine
     return spliters
 
-refinePath = "./refineReviews/";
+refinePath = "./refineReviews/"
+phoneticsReviewsPath = "./phoneticsReviews/"
 reviewsList = os.listdir(refinePath)
 
 reviews = [line.rstrip('\n') for line in open(refinePath+reviewsList[0])]
-allReviews = getReviews(reviews)
-
+showLines(reviews)
+# allReviews = getReviews(reviews)
+#
+#
 # for review in allReviews:
 #     for rev in splitByNewLine(review):
 #         print(rev)
 #         sentimentAnalysis(rev)
 #         print()
-
-sentimentAnalysis(splitByNewLine(allReviews[0])[0])
+#
+#
+#
+# showLines(textBlobresult)
+# print()
+# showLines(vaderSentimentResult)
--- python3/RefineData.py
+++ python3/RefineData.py
@@ -1,11 +1,46 @@
 import os
 import io
 from bs4 import BeautifulSoup
+import re
 
 def showList(colList):
     for n in colList:
         print(n)
     print()
+
+def removeLinks(line):
+    text = re.sub(r'http\S+', '', line)
+    return text
+
+def removeSpecialChar(line):
+    text = re.sub(r'[?|$|.|!|*]', r'', line)
+    return text
+
+def getUniqueReviews(lines):
+    uniqueLines = []
+    foundNewLine = False
+    for line in lines:
+        if len(line.strip()) == 0 and foundNewLine is False:
+            uniqueLines.append("")
+            foundNewLine = True
+        elif line not in uniqueLines:
+            line = removeLinks(line)
+            line = removeSpecialChar(line)
+            uniqueLines.append(line)
+            foundNewLine = False
+    return uniqueLines
+
+def getReviews(lines):
+    lines = getUniqueReviews(lines)
+    review = ""
+    reviewList = []
+    for line in lines:
+        if len(line.strip()) == 0:
+            reviewList.append(review)
+            review = ""
+        else:
+            review += line + "\n"
+    return reviewList
 
 def removeTags(lines):
     return BeautifulSoup(lines, "lxml").text
@@ -24,4 +59,5 @@ reviewsList = os.listdir(folderPath)
 
 for li in reviewsList:
     lines = [line.rstrip('\n') for line in open(folderPath+li)]
+    lines = getReviews(lines)
     writeListInFile(fileName=refinePath+li, mode="a", writeList=lines)
